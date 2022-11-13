69bcf227bc45ad0a84204cb4aa0e7714eefd01d4
Дом <ira-rostov80@mail.ru>
2022-11-13 23:02:09 +0300

    первый коммит 11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
-public class Main {
+package exceptions;
+
+import Exceptions.CredentialsValidator;
+
+public class HomeworkExceptions {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("checkCredentials");
+
+        (login:"login", password:"password", confirmpassword:"password");
+        (login:"login", password:"password", confirmpassword:"password");
+        (login:"login", password:"password", confirmpassword:"password");
+        (login:"login", password:"password", confirmpassword:"password");
+        (login:"login", password:"password", confirmpassword:"password");
+        (login:"login", password:"password", confirmpassword:"password");
+        (login:"login", password:"password", confirmpassword:"password");
+    }
+
+    public static boolean checCredentials(String login, String password, String confirmPassword);
+    try
+
+    {
+        return CredentialsValidator.validateCredentials(login, password, confirmpassword);
+    } catch(
+    WrongLoginException e)
+
+    {
+        System.out.println("Ошибка при валидации логина:" + е.getMessage());
+        return false;
+    } catch(
+    WrongLoginException e)
+
+    {
+        System.out.println("Ошибка при валидации пароля:" + e.getMessage());
+        return false;
+    }
+}
+
+
     }
 }
